1a218ad69ce777c3db94d18aa21905f42c6834e4
carlosperate <carlosperate@users.noreply.github.com>
2020-12-29 23:34:53 +0000

    Break down Python script into download and update os image.

--- add_autologin.py
+++ add_autologin.py
@@ -1,31 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""Run a Raspberry PI OS image with Docker and QEMU to configure autologin."""
+"""Run a Raspberry PI OS image with Docker and QEMU to customise it."""
 import os
 import sys
 import uuid
 import shutil
-from zipfile import ZipFile
 
 import pexpect
-import requests
 
 
 ###############################################################################
 # Configuration data start
 
-# URL of the Raspberry Pi OS Lite zip file to download and check
-# Find options in https://downloads.raspberrypi.org/raspios_lite_armhf/images/
-OS_IMAGE_ZIP = "https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2020-08-24/2020-08-20-raspios-buster-armhf-lite.zip"
-OS_IMAGE_SHA = "https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2020-08-24/2020-08-20-raspios-buster-armhf-lite.zip.sha256"
-OS_IMAGE_SHA_TYPE = "256"
+# If this script is run on its own, select here what features to update
+AUTOLOGIN = True
 
 # Configuration data end
 ###############################################################################
-
-IMAGE_SAVE_LOCATION = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "rpiosimage"
-)
 
 DOCKER_IMAGE = "lukechilds/dockerpi:vm"
 DOCKER_CONTAINER_NAME= "rpi-os-autologin-{}".format(str(uuid.uuid4())[:8])
@@ -45,58 +36,20 @@ ExecStart=-/sbin/agetty --autologin {} --keep-baud 115200,38400,9600 %I $TERM
 """.format(RPI_OS_USERNAME)
 
 
-def download_image_zip(zip_url):
-    """Download a zip image from the internet.
-
-    :param zip_url: URL to the zip file to download.
-    :return: Absolute path to the downloaded zip file.
-    """
-    if not os.path.exists(IMAGE_SAVE_LOCATION):
-        os.makedirs(IMAGE_SAVE_LOCATION)
-    zip_img_filename = os.path.join(IMAGE_SAVE_LOCATION, zip_url.split('/')[-1])
-
-    response = requests.get(zip_url, stream=True)
-    if response.status_code == 200:
-        with open(zip_img_filename, 'wb') as f:
-            for i, chunk in enumerate(response.iter_content(chunk_size=10*1024*1024)):
-                if chunk:
-                    print("\t-> Downloaded {}0MB...".format(i), end='\r')
-                    f.write(chunk)
-            print("Download done!                  ")
-    else:
-        raise Exception("Could not reach the zip URL, error code {}: {}".format(
-            response.status_code, zip_url
-        ))
-    return os.path.abspath(zip_img_filename)
-
-
-def unzip_image(zip_path):
-    """Unzips a file with a img file inside.
-
-    :param zip_path: Path to the zip file to uncompress.
-    :raises Exception: If there is no .img file inside the zip.
-    :return: Absolute path to an uncompressed .img file from the zip.
-    """
-    if not os.path.exists(IMAGE_SAVE_LOCATION):
-        os.makedirs(IMAGE_SAVE_LOCATION)
-    with ZipFile(zip_path, 'r') as z:
-        name_list = z.namelist()
-        z.extractall(path=IMAGE_SAVE_LOCATION)
-    for file_name in name_list:
-        if file_name.endswith('.img'):
-            return os.path.abspath(os.path.join(IMAGE_SAVE_LOCATION, file_name))
-    raise Exception("Could not find img file inside zip")
-
-
-def update_os_image(img_path):
+def launch_docker_spawn(img_path):
     """Creates a copy of the provided Raspberry Pi OS Lite image and runs it in
     QEMU inside a Docker container to configure autologin.
 
     :param img_path: Path to the Raspberry Pi OS Lite image to update.
     """
+    img_path = os.path.abspath(img_path)
+    if not os.path.isfile(img_path):
+        raise Exception("Provided OS file cannot be found: {}".format(img_path))
     if not img_path.endswith(".img"):
-        raise Exception("Provided OS .img file does not have the right extension.")
+        raise Exception("Provided OS .img file does not have the right extension: {}".format(img_path))
+
     original_img = img_path
+    # TODO: .replace will cause issues if `.img` is present somewhere else in path
     new_img = img_path.replace(".img", "-autologin.img")
     shutil.copyfile(img_path, new_img)
     new_img = os.path.abspath(new_img)
@@ -112,37 +65,52 @@ def update_os_image(img_path):
     ])
     print("Docker cmd: {}".format(docker_cmd))
 
+    child = pexpect.spawn(docker_cmd, timeout=600, encoding='utf-8')
+    child.logfile = sys.stdout
+    return child
+
+
+def login(child):
+    child.expect_exact("raspberrypi login: ")
+    child.sendline(RPI_OS_USERNAME)
+    child.expect_exact("Password: ")
+    child.sendline(RPI_OS_PASSWORD)
+    child.expect_exact(BASH_PROMPT)
+
+
+def add_autologin(child):
+    # Setup a service to configure autologin in ttyAMA0, which is what QEMU uses
+    child.sendline("sudo mkdir -pv /etc/systemd/system/serial-getty@ttyAMA0.service.d")
+    child.expect_exact(BASH_PROMPT)
+    child.sendline('echo -en "{}" >> autologin.conf'.format(
+        SERIAL_TTY_SERVICE_AUTOLOGIN_CONF.replace("\n", "\\n"))
+    )
+    child.expect_exact(BASH_PROMPT)
+    child.sendline("sudo mv autologin.conf /etc/systemd/system/serial-getty@ttyAMA0.service.d/autologin.conf")
+    child.expect_exact(BASH_PROMPT)
+    child.sendline("sudo systemctl enable serial-getty@ttyAMA0.service")
+    child.expect_exact(BASH_PROMPT)
+    # Setup a service to autologin in the default tty
+    child.sendline("sudo mkdir -pv /etc/systemd/system/getty@tty1.service.d")
+    child.expect_exact(BASH_PROMPT)
+    child.sendline('echo -en "{}" >> autologin.conf'.format(
+        TTY_SERVICE_AUTOLOGIN_CONF.replace("\n", "\\n"))
+    )
+    child.expect_exact(BASH_PROMPT)
+    child.sendline("sudo mv autologin.conf /etc/systemd/system/getty@tty1.service.d/autologin.conf")
+    child.expect_exact(BASH_PROMPT)
+    child.sendline("sudo systemctl enable getty@tty1.service")
+    child.expect_exact(BASH_PROMPT)
+
+
+def run_edits(img_path, needs_login=True, autologin=None):
+    print("Staring Raspberry Pi OS customisation: {}".format(img_path))
     try:
-        child = pexpect.spawn(docker_cmd, timeout=600, encoding='utf-8')
-        child.logfile = sys.stdout
-        # Login
-        child.expect_exact("raspberrypi login: ")
-        child.sendline(RPI_OS_USERNAME)
-        child.expect_exact("Password: ")
-        child.sendline(RPI_OS_PASSWORD)
-        child.expect_exact(BASH_PROMPT)
-        # Setup a service to configure autologin in ttyAMA0, which is what QEMU uses
-        child.sendline("sudo mkdir -pv /etc/systemd/system/serial-getty@ttyAMA0.service.d")
-        child.expect_exact(BASH_PROMPT)
-        child.sendline('echo -en "{}" >> autologin.conf'.format(
-            SERIAL_TTY_SERVICE_AUTOLOGIN_CONF.replace("\n", "\\n"))
-        )
-        child.expect_exact(BASH_PROMPT)
-        child.sendline("sudo mv autologin.conf /etc/systemd/system/serial-getty@ttyAMA0.service.d/autologin.conf")
-        child.expect_exact(BASH_PROMPT)
-        child.sendline("sudo systemctl enable serial-getty@ttyAMA0.service")
-        child.expect_exact(BASH_PROMPT)
-        # Setup a service to autologin in the default tty
-        child.sendline("sudo mkdir -pv /etc/systemd/system/getty@tty1.service.d")
-        child.expect_exact(BASH_PROMPT)
-        child.sendline('echo -en "{}" >> autologin.conf'.format(
-            TTY_SERVICE_AUTOLOGIN_CONF.replace("\n", "\\n"))
-        )
-        child.expect_exact(BASH_PROMPT)
-        child.sendline("sudo mv autologin.conf /etc/systemd/system/getty@tty1.service.d/autologin.conf")
-        child.expect_exact(BASH_PROMPT)
-        child.sendline("sudo systemctl enable getty@tty1.service")
-        child.expect_exact(BASH_PROMPT)
+        child = launch_docker_spawn(img_path)
+        if needs_login:
+            login(child)
+        if autologin or (autologin is None and AUTOLOGIN):
+            add_autologin(child)
         # We are done, let's exit
         child.sendline("sudo shutdown now")
         child.expect(pexpect.EOF)
@@ -170,14 +138,6 @@ def update_os_image(img_path):
                 print('! Docker container was already stopped.')
 
 
-def main():
-    print("Downloading OS image: {}".format(OS_IMAGE_ZIP))
-    zip_path = download_image_zip(OS_IMAGE_ZIP)
-    print("Unzipping OS image: {}".format(zip_path))
-    img_path = unzip_image(zip_path)
-    print("Configuring OS to autologin: {}".format(img_path))
-    update_os_image(img_path)
-
-
 if __name__ == "__main__":
-    main()
+    # We only use the first argument to receive a path to the .img file
+    run_edits(sys.argv[1])
---
+++ download_os.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Download a Raspberry PI OS image."""
+import os
+from zipfile import ZipFile
+
+import requests
+
+
+###############################################################################
+# Configuration data start
+
+# URL of the Raspberry Pi OS Lite zip file to download and check
+# Find options in https://downloads.raspberrypi.org/raspios_lite_armhf/images/
+OS_IMAGE_ZIP = "https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2020-08-24/2020-08-20-raspios-buster-armhf-lite.zip"
+OS_IMAGE_SHA = "https://downloads.raspberrypi.org/raspios_lite_armhf/images/raspios_lite_armhf-2020-08-24/2020-08-20-raspios-buster-armhf-lite.zip.sha256"
+OS_IMAGE_SHA_TYPE = "256"
+
+# Configuration data end
+###############################################################################
+
+IMAGE_SAVE_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "rpiosimage"
+)
+
+
+def download_image_zip(zip_url=OS_IMAGE_ZIP):
+    """Download a zip image from the internet.
+
+    :param zip_url: URL to the zip file to download.
+    :return: Absolute path to the downloaded zip file.
+    """
+    print("Downloading OS image: {}".format(zip_url))
+
+    if not zip_url.startswith("http") or not zip_url.endswith(".zip"):
+        raise Exception("Provided URL must be a zip file.")
+
+    if not os.path.exists(IMAGE_SAVE_LOCATION):
+        os.makedirs(IMAGE_SAVE_LOCATION)
+    zip_img_filename = os.path.join(IMAGE_SAVE_LOCATION, zip_url.split('/')[-1])
+
+    response = requests.get(zip_url, stream=True)
+    if response.status_code == 200:
+        with open(zip_img_filename, 'wb') as f:
+            for i, chunk in enumerate(response.iter_content(chunk_size=10*1024*1024)):
+                if chunk:
+                    print("\t-> Downloaded {}0MB...".format(i), end='\r')
+                    f.write(chunk)
+            print("Download done!                  ")
+    else:
+        raise Exception("Could not reach the zip URL, error code {}: {}".format(
+            response.status_code, zip_url
+        ))
+    return os.path.abspath(zip_img_filename)
+
+
+def unzip_image(zip_path):
+    """Unzips a file with a img file inside.
+
+    :param zip_path: Path to the zip file to uncompress.
+    :raises Exception: If there is no .img file inside the zip.
+    :return: Absolute path to an uncompressed .img file from the zip.
+    """
+    print("Unzipping OS image: {}".format(zip_path))
+
+    if not os.path.exists(IMAGE_SAVE_LOCATION):
+        os.makedirs(IMAGE_SAVE_LOCATION)
+    with ZipFile(zip_path, 'r') as z:
+        name_list = z.namelist()
+        z.extractall(path=IMAGE_SAVE_LOCATION)
+    for file_name in name_list:
+        if file_name.endswith('.img'):
+            return os.path.abspath(os.path.join(IMAGE_SAVE_LOCATION, file_name))
+    raise Exception("Could not find img file inside zip")
+
+
+def main(img_zip_url=None):
+    zip_path = download_image_zip(img_zip_url)
+    img_path = unzip_image(zip_path)
+
+
+if __name__ == "__main__":
+    # We only use the first argument to receive a URL to the .img file
+    main(sys.argv[1])
